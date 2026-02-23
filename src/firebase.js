2b3d3566b07cc489b09fc98081bd15af82684518
Bhavish-rai <raibhavish007@gmail.com>
2026-02-24 00:40:01 +0530

    Clean final LifeArc setup without .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,4 @@
 
-
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCGowP90N3mBxvUiIanCXTLX15dBVileFg",
-//   authDomain: "donor-app-31b49.firebaseapp.com",
-//   projectId: "donor-app-31b49",
-//   storageBucket: "donor-app-31b49.firebasestorage.app",
-//   messagingSenderId: "562030151188",
-//   appId: "1:562030151188:web:b80f84ee6ba14b7f318901"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
-// export const db = getFirestore(app);
-
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
